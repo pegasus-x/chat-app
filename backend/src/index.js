79929b433023d98fdbc798992c4eff74cb15bdca
Rati Ranjan <135224384+pegasus-x@users.noreply.github.com>
2025-06-24 21:33:40 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,13 +30,14 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// Serve frontend in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
+
 
 // Setup server and socket.io
 const { server } = setupSocket(app);
