c4cc9cf2d854a1a5df1681b4d7fc261da57b300f
Rati Ranjan <135224384+pegasus-x@users.noreply.github.com>
2025-06-27 23:26:40 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(cookieParser());
 app.use(
   cors({
