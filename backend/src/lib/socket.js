a6cf29eb6fdbd64e30dd14325d0f4e6eb809cbdb
Rati Ranjan <135224384+pegasus-x@users.noreply.github.com>
2025-06-27 16:34:40 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,42 +1,38 @@
 import { Server } from "socket.io";
 import http from "http";
+import express from "express";
 
-const userSocketMap = {};
-let io; 
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "https://chat-app-70bc.onrender.com",
+    methods: ["GET", "POST"],
+  },
+});
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-function setupSocket(app) {
-  const server = http.createServer(app);
+// used to store online users
+const userSocketMap = {}; // {userId: socketId}
 
-  io = new Server(server, {
-    cors: {
-      origin: [
-        "http://localhost:5173",              // For local development
-        "https://chat-app-70bc.onrender.com" // For production
-      ],
-      credentials: true,
-    },
-  });
-  io.on("connection", (socket) => {
-    console.log("A user connected:", socket.id);
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
 
-    const userId = socket.handshake.query.userId;
-    if (userId) userSocketMap[userId] = socket.id;
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
 
+  // io.emit() is used to send events to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+    delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-    socket.on("disconnect", () => {
-      console.log("A user disconnected:", socket.id);
-      delete userSocketMap[userId];
-      io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    });
   });
+});
 
-  return { server, io };
-}
-
-export { setupSocket, io };
-
+export { io, app, server };
